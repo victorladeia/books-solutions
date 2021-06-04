0ed5284f853be9b9c61f89cb4e199d059aa61ef0
Victor Ladeia <ladeiavictor@gmail.com>
2021-06-04 21:41:53 +0000

    Submitting exercide 3.23

---
+++ c-como-programar/capitulo3/exercicios/ex3_23.c
@@ -0,0 +1,14 @@
+// Printing number by a loop
+
+#include <stdio.h>
+
+int main(void)
+{
+    for (int i = 1; i <= 10; i++)
+    {
+        printf("%i   ", i);
+    }
+    printf("\n");
+
+    return 0;
+}
